6f0521738d81982f94299016e6c7bdb11c1623e6
Mdajaj123 <137004218+Mdajaj123@users.noreply.github.com>
2023-10-23 16:19:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-//your JS code here. If required.
-setInterval(updateTime,1000);
-function updateTime(){
+// //your JS code here. If required.
+// setInterval(updateTime,1000);
+// function updateTime(){
+// 	timer.innerText=new Date();
+// }
+ setInterval ((updateTime)=>{
 	timer.innerText=new Date();
-}
+},1000);
